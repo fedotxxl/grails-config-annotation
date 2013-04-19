7db64fc632c7f6796070033d20d227009a2dc41c
fedor.belov <fedor.belov@mail.ru>
2013-04-19 23:04:43 +0400

    cleared project

--- GrailsConfigAnnotationGrailsPlugin.groovy
+++ GrailsConfigAnnotationGrailsPlugin.groovy
@@ -1,38 +1,25 @@
-import com.MySuperService
 import com.tenlittleniggers.grails.config.GrailsConfigAnnotationHandler
-import com.tenlittleniggers.grails.config.GrailsConfigAnnotationHandler
-import com.tenlittleniggers.grails.config.GrailsConfigAnnotationInitializer
 import com.tenlittleniggers.grails.config.GrailsConfigBeanPostProcessor
-import org.codehaus.groovy.grails.commons.GrailsApplication
-import org.springframework.core.io.FileSystemResource
-
-import java.lang.annotation.Annotation
-import java.lang.reflect.Field
 
 class GrailsConfigAnnotationGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.0"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.1 > *"
+    def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
+    def pluginExcludes = []
 
-    // TODO Fill in these fields
     def title = "Grails Config Annotation Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def author = "fedor.belov"
+    def authorEmail = "fedor.belov@mail.ru"
     def description = '''\
 Brief summary/description of the plugin.
 '''
 
     //watch for all scss file changes
     def watchedResources = ["file:./**/*.groovy"]
-
-//    def observe = ['*']
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/grails-config-annotation"
@@ -42,35 +29,6 @@ Brief summary/description of the plugin.
     def doWithSpring = {
         grailsConfigBeanPostProcessor(GrailsConfigBeanPostProcessor)
     }
-
-    def doWithApplicationContext = { applicationContext ->
-//        //init handler
-//        println GrailsConfigAnnotationHandler.calls
-//        println GrailsConfigAnnotationHandler.times
-//        GrailsConfigAnnotationInitializer.application = application
-//
-//        //update beans
-//        application.allArtefacts.each { clazz ->
-//            GrailsConfigAnnotationHandler.initClass(clazz, application)
-//        }
-    }
-
-//    private updateAnnotatedConfigPropertiesOfClass(def clazz, def application) {
-//        def annotated = GrailsConfigAnnotationHandler.getGrailsConfigAnnotationsByFields(clazz)
-//        if (annotated) {
-//            def beans = application.mainContext.getBeansOfType(clazz).values()
-//            beans.each { bean ->
-//                annotated.each { an ->
-//                    def field = an.key, annotation = an.value
-//                    //update bean field value
-//                    GrailsConfigAnnotationHandler.updateAnnotatedConfigProperty(bean, application.config.flatten(), field, annotation)
-//                    //save this info for config reload
-//                    annotatedBeans << [bean, field, annotation]
-//                    annotatedClasses << clazz
-//                }
-//            }
-//        }
-//    }
 
     def onChange = { event ->
         try {
